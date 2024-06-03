a0a0a6246ecd3ab7b4f7a717a564ced299b5f2c2
Elizar <elizar.batin@yandex.ru>
2024-06-03 20:26:02 +0300

    Some updates

--- lua/simplerunner/init.lua
+++ lua/simplerunner/init.lua
@@ -1,31 +1,15 @@
 local M = {}
 
 
-function my_help_opener(help_str)
-  local win_id
-  -- Iterate over the existing windows. If any of them has `ft=help` or `ft=man`, then
-  -- set `win_id` to point to that window. This is straightforward today.
-
-  local win_open_spec
-  if win_id then
-    win_open_spec = {
-      win_id = win_id,
-      type = 'open_in_existing',
-    }
-  else
-    win_open_spec = {
-      type = 'vsplit_in_editor',
-      dir = 'right'
-    }
-  end
-
-  vim.api.open_buffer(vim.api.make_help_buffer(help_str), win_open_spec)
-end
-
 function M.setup(opts)
   opts = opts or {}
   print("Hello world!")
-  my_help_opener("Hello world")
+  local buf = vim.api.nvim_create_buf(false, true)
+  vim.api.nvim_buf_set_lines(buf, 0, -1, true, ["test", "text"])
+
+  local opts = {'relative': 'cursor', 'width': 10, 'height': 2, 'col': 0,
+      \ 'row': 1, 'anchor': 'NW', 'style': 'minimal'}
+  local win = vim.api.nvim_open_win(buf, 0, opts)
 end
 
 return M

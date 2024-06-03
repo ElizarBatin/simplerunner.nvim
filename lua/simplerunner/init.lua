6db98cf6a21fc56f189e750297b8129a4397423b
Elizar <elizar.batin@yandex.ru>
2024-06-03 20:32:08 +0300

    Some updates

--- lua/simplerunner/init.lua
+++ lua/simplerunner/init.lua
@@ -7,7 +7,7 @@ function M.setup(opts)
   local buf = vim.api.nvim_create_buf(false, true)
   vim.api.nvim_buf_set_lines(buf, 0, -1, true, {"test", "text"})
 
-  local o = {'relative': 'cursor', 'width': 10, 'height': 2, 'col': 0, 'row': 1, 'anchor': 'NW', 'style': 'minimal'}
+  local o = {"relative" = 'cursor', 'width' = 10, 'height' =  2, 'col' = 0, 'row' = 1, 'anchor' = 'NW', 'style' = 'minimal'}
   local win = vim.api.nvim_open_win(buf, 0, o)
 end
 

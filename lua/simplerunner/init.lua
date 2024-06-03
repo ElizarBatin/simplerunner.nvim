2adf3c7f0ec8bfca9da24f7d183c806361fee1ef
Elizar <elizar.batin@yandex.ru>
2024-06-03 20:59:21 +0300

    Some updates

--- lua/simplerunner/init.lua
+++ lua/simplerunner/init.lua
@@ -1,16 +1,28 @@
 local M = {}
 
+function split_string(input, sep)
+  if sep == nil then
+    sep = "%s"
+  end
+  local t = {}
+  for str in string.gmatch(input, "([^"..sep.."]+)") do
+    table.insert(t, str)
+  end
+  return t
+end
+
 
 function M.setup(opts)
   opts = opts or {}
   print("Hello world!")
   local handle = io.popen("whoami")
   local result = handle:read("*a")
-  print(result)
   handle:close()
 
+  result = split_string(result, "\n")
+
   local buf = vim.api.nvim_create_buf(false, true)
-  vim.api.nvim_buf_set_lines(buf, 0, -1, true, {result, "text"})
+  vim.api.nvim_buf_set_lines(buf, 0, -1, true, result)
   vim.api.nvim_buf_set_name(buf, "SimpleRunner")
 
   local o = {

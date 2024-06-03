7e48477051e2e663c977ecb7abcc1acde00a7583
Elizar <elizar.batin@yandex.ru>
2024-06-03 20:44:41 +0300

    Some updates

--- lua/simplerunner/init.lua
+++ lua/simplerunner/init.lua
@@ -5,6 +5,7 @@ function M.setup(opts)
   opts = opts or {}
   print("Hello world!")
   local buf = vim.api.nvim_create_buf(false, true)
+  vim.bo[buf].readonly = true
   vim.api.nvim_buf_set_lines(buf, 0, -1, true, {"test", "text"})
 
   local o = {
